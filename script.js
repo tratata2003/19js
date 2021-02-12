e1d627a5bd1356be97202d5b3b0092288dfe9f8c
Milka Vladislav <58935008+tratata2003@users.noreply.github.com>
2021-02-12 18:00:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,33 +12,36 @@ var moveBanner = function(){
     banner.style.right = (banner.style.right == "0px") ? "-450px" : "0px";
 }
 
-
-list = document.getElementById("list");
-list.onclick = function(e) {
-var tag = e.target;
-  tag.remove();
-}
-
-
-field.onclick = function(event){
-    let fieldCoords = this.getBoundingClientRect();
-
-    let ballCoords = {
-      top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
-      left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
-    };
-
-    if (ballCoords.top < 0) ballCoords.top = 0;
-    if (ballCoords.left < 0) ballCoords.left = 0;
-
-    if (ballCoords.left + ball.clientWidth > field.clientWidth) {
-      ballCoords.left = field.clientWidth - ball.clientWidth;
+scr = function(){
+  try{
+    list.addEventListener("click", (e) => {
+      var tag = e.target;
+      tag.remove();
+    })
+  }catch(error){}
+  
+  try {
+    field.onclick = function(event){
+      let fieldCoords = this.getBoundingClientRect();
+  
+      let ballCoords = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+      };
+  
+      if (ballCoords.top < 0) ballCoords.top = 0;
+      if (ballCoords.left < 0) ballCoords.left = 0;
+  
+      if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+      }
+  
+      if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+      }
+  
+      ball.style.left = ballCoords.left + 'px';
+      ball.style.top = ballCoords.top + 'px';
     }
-
-    if (ballCoords.top + ball.clientHeight > field.clientHeight) {
-      ballCoords.top = field.clientHeight - ball.clientHeight;
-    }
-
-    ball.style.left = ballCoords.left + 'px';
-    ball.style.top = ballCoords.top + 'px';
+  } catch(error){}
 }
